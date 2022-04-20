22e46fa9437e42255b9cbd78d50f520f071eaa0a
Huy Tran <thtran.082@gmail.com>
2022-04-21 01:30:00 +0700

    chore: remove redundant files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './pages/error/error.component';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  // { path: '', pathMatch: 'full', redirectTo: 'not-found' },
-  // { path: 'not-found', component: ErrorComponent },
   {
     path: '',
     loadChildren: () => import('./shell/core.module').then((m) => m.CoreModule),
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './pages/error/error.component';
 import { AppRoutingModule } from "./app-routing.module";
-import { HeaderComponent } from './pages/layout/header/header.component';
-import { FooterComponent } from './pages/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     AppRoutingModule,
